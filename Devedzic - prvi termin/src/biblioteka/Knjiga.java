0c513c1abba7c9142eb2a0bdad2c2076f135aa24
jelenamarjanovic9114 <jmarjanovic@hotmail.com>
2016-03-10 15:16:18 +0100

    Dodali smo atribute u klasu Knjiga Get i set metode

--- Devedzic - prvi termin/src/biblioteka/Knjiga.java
+++ Devedzic - prvi termin/src/biblioteka/Knjiga.java
@@ -1,5 +1,59 @@
 package biblioteka;
 
+//prvo treba add to index 
+//pa onda commit
+
 public class Knjiga {
+	private String naslov;
+	private long isbn;
+	private String autor;
+	private int brojStrana;
+	private String izdavac;
+	private String izdanje;
+	private int godinaIzdavanja;
+	public String getNaslov() {
+		return naslov;
+	}
+	public void setNaslov(String naslov) {
+		this.naslov = naslov;
+	}
+	public long getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(long isbn) {
+		this.isbn = isbn;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public int getBrojStrana() {
+		return brojStrana;
+	}
+	public void setBrojStrana(int brojStrana) {
+		this.brojStrana = brojStrana;
+	}
+	public String getIzdavac() {
+		return izdavac;
+	}
+	public void setIzdavac(String izdavac) {
+		this.izdavac = izdavac;
+	}
+	public String getIzdanje() {
+		return izdanje;
+	}
+	public void setIzdanje(String izdanje) {
+		this.izdanje = izdanje;
+	}
+	public int getGodinaIzdavanja() {
+		return godinaIzdavanja;
+	}
+	public void setGodinaIzdavanja(int godinaIzdavanja) {
+		this.godinaIzdavanja = godinaIzdavanja;
+	}
+	
+	
 	
 }
